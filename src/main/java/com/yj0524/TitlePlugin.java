8fc0ad2b8ae5da4152443ed05ae2b6397b0f0da4
yj0524 <jeonyunjae_kr@naver.com>
2023-02-06 15:40:09 +0900

    1.0.2

--- src/main/java/com/yj0524/TitlePlugin.java
+++ src/main/java/com/yj0524/TitlePlugin.java
@@ -1,9 +1,16 @@
 package com.yj0524;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 public final class TitlePlugin extends JavaPlugin {
 
@@ -30,14 +37,66 @@ public final class TitlePlugin extends JavaPlugin {
                 String title = args[1];
                 String subtitle = args[2];
 
-                Player target = getServer().getPlayer(playerName);
-                if (target != null) {
-                    target.sendTitle(title, subtitle);
-                    return true;
-                } else {
-                    sender.sendMessage("Player not found: " + playerName);
-                    return false;
+                List<Entity> targets = new ArrayList<>();
+                switch (playerName) {
+                    case "@a":
+                        targets = Bukkit.getOnlinePlayers().stream().collect(Collectors.toList());
+                        break;
+                    case "@e":
+                        targets = Bukkit.getWorlds().stream().flatMap(w -> w.getEntities().stream()).collect(Collectors.toList());
+                        break;
+                    case "@p":
+                        targets.add((Entity) sender);
+                        break;
+                    case "@r":
+                        List<Player> players = Bukkit.getOnlinePlayers().stream().collect(Collectors.toList());
+                        targets.add(players.get(new Random().nextInt(players.size())));
+                        break;
+                    case "@s":
+                        if (sender instanceof Entity) {
+                            targets.add((Entity) sender);
+                        } else {
+                            sender.sendMessage("The sender is not an entity.");
+                            return false;
+                        }
+                        break;
+                    default:
+                        if (playerName.startsWith("@a[tag=")) {
+                            String tag = playerName.substring(7, playerName.length() - 1);
+                            targets = Bukkit.getOnlinePlayers().stream()
+                                    .filter(p -> p.getScoreboardTags().contains(tag))
+                                    .collect(Collectors.toList());
+                            if (targets.isEmpty()) {
+                                sender.sendMessage("No player found with the tag " + tag);
+                                return false;
+                            }
+                        } else if (playerName.startsWith("@e[tag=")) {
+                            String tag = playerName.substring(7, playerName.length() - 1);
+                            targets = Bukkit.getWorlds().stream()
+                                    .flatMap(w -> w.getEntities().stream())
+                                    .filter(e -> e.getScoreboardTags().contains(tag))
+                                    .collect(Collectors.toList());
+                            if (targets.isEmpty()) {
+                                sender.sendMessage("No entity found with the tag " + tag);
+                                return false;
+                            }
+                        } else {
+                            Player target = Bukkit.getPlayer(playerName);
+                            if (target != null) {
+                                targets.add(target);
+                            } else {
+                                sender.sendMessage("Player not found: " + playerName);
+                                return false;
+                            }
+                        }
                 }
+
+                for (Entity target : targets) {
+                    if (target instanceof Player) {
+                        ((Player) target).sendTitle(title, subtitle);
+                    }
+                }
+                return true;
             } else {
                 sender.sendMessage("Usage: /titles <player> <title> <subtitle>");
                 return false;
